2ee44177d5b5aa39d0f98c0c017df4c89e83a8b1
mortuusars <mortuusars@gmail.com>
2023-06-08 06:22:07 +0300

    Initial commit.

---
+++ src/main/java/io/github/mortuusars/exposure/Exposure.java
@@ -0,0 +1,20 @@
+package io.github.mortuusars.exposure;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Exposure.ID)
+public class Exposure {
+    public static final String ID = "exposure";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Exposure() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
