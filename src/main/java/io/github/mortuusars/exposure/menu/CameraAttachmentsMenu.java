2a043421b111f9f0094416e659040d5e0b5eb487
mortuusars <mortuusars@gmail.com>
2023-07-26 14:23:57 +0300

    Camera model.

--- src/main/java/io/github/mortuusars/exposure/menu/CameraAttachmentsMenu.java
+++ src/main/java/io/github/mortuusars/exposure/menu/CameraAttachmentsMenu.java
@@ -47,7 +47,6 @@ public class CameraAttachmentsMenu extends AbstractContainerMenu {
 
         this.attachmentSlots = attachmentSlots;
 
-
         //Player Inventory
         for (int row = 0; row < 3; row++) {
             for (int column = 0; column < 9; column++) {
