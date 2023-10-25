c595bc69de87be6f10d3ddf10336d8a23e0dd995
mortuusars <mortuusars@gmail.com>
2023-10-25 03:18:53 +0300

    Structures in frame

--- src/main/java/io/github/mortuusars/exposure/network/packet/CameraInHandAddFrameServerboundPacket.java
+++ src/main/java/io/github/mortuusars/exposure/network/packet/CameraInHandAddFrameServerboundPacket.java
@@ -3,16 +3,29 @@ package io.github.mortuusars.exposure.network.packet;
 import com.google.common.base.Preconditions;
 import io.github.mortuusars.exposure.item.CameraItem;
 import io.github.mortuusars.exposure.network.Packets;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public record CameraInHandAddFrameServerboundPacket(InteractionHand hand, CompoundTag frame) {
@@ -51,7 +64,36 @@ public record CameraInHandAddFrameServerboundPacket(InteractionHand hand, Compou
         if (!(itemInHand.getItem() instanceof CameraItem cameraItem))
             throw new IllegalStateException("Item in hand in not a Camera.");
 
+        addStructuresInfo(player);
+
         cameraItem.exposeFilmFrame(itemInHand, frame);
+        player.setItemInHand(hand, itemInHand);
         return true;
+    }
+
+    private void addStructuresInfo(@NotNull ServerPlayer player) {
+        Map<Structure, LongSet> allStructuresAt = player.getLevel().structureManager().getAllStructuresAt(player.blockPosition());
+
+        List<Structure> inside = new ArrayList<>();
+
+        for (Structure structure : allStructuresAt.keySet()) {
+            StructureStart structureAt = player.getLevel().structureManager().getStructureAt(player.blockPosition(), structure);
+            if (structureAt.isValid()) {
+                inside.add(structure);
+            }
+        }
+
+        Registry<Structure> structures = player.getLevel().registryAccess().registryOrThrow(BuiltinRegistries.STRUCTURES.key());
+        ListTag structuresTag = new ListTag();
+
+        for (Structure structure : inside) {
+            ResourceLocation key = structures.getKey(structure);
+            if (key != null)
+                structuresTag.add(StringTag.valueOf(key.toString()));
+        }
+
+        if (structuresTag.size() > 0) {
+            frame.put("Structures", structuresTag);
+        }
     }
 }
