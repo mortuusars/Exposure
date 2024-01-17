dbd5faf4f992f146ff2719d1338d75da3fe1e26e
mortuusars <mortuusars@gmail.com>
2024-01-18 01:19:41 +0200

    new box

--- common/src/main/java/io/github/mortuusars/exposure/client/gui/screen/TextTestScreen.java
+++ common/src/main/java/io/github/mortuusars/exposure/client/gui/screen/TextTestScreen.java
@@ -1,6 +1,7 @@
 package io.github.mortuusars.exposure.client.gui.screen;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import io.github.mortuusars.exposure.client.gui.screen.element.NewTextBox;
 import io.github.mortuusars.exposure.client.gui.screen.element.TextBlock;
 import io.github.mortuusars.exposure.client.gui.screen.element.TextBox;
 import net.minecraft.client.Minecraft;
@@ -12,58 +13,30 @@ import net.minecraft.network.chat.Component;
 
 public class TextTestScreen extends Screen {
 
-    private MultiLineEditBox textBox;
+    private NewTextBox textBox;
 
     public TextTestScreen() {
         super(Component.empty());
     }
 
+    private String text = "initial";
+
     @Override
     protected void init() {
-//        textBox = new TextBox(Minecraft.getInstance().font, 50, 50, 114, 27)
-//                .setFontColor(0xFFb59774);
-//
-//        textBox.setText("initial");
-//        textBox.setCursorToEnd();
-//        textBox.selectionColor = 0xFF8888FF;
-//        textBox.selectionUnfocusedColor = 0xFFBBBBFF;
-//        textBox.fontUnfocusedColor = 0xFFb59774;
-//
-//        addRenderableWidget(textBox);
+        textBox = new NewTextBox(font, width / 2 - 50, height / 2 - 20, 100, 40, () -> text, t -> text = t);
 
-        Button button1 = Button.builder(Component.literal("Button"), button -> {
-                })
-                .pos(this.width / 2 - 20 / 2, 100)
-                .size(40, 30)
-                .build();
 
-        addRenderableWidget(button1);
 
-        MultiLineEditBox note = new MultiLineEditBox(font, 50, 50, 114, 27, Component.literal("Note"), Component.empty());
-        addRenderableWidget(note);
-        textBox = note;
-
-        TextBlock textBlock = new TextBlock(font, 50, 50, 114, 27, Component.literal("Testing a looooooooooooooooooooong tooltip. textBox.setText(\"initial\");\n" +
-                "//        textBox.setCursorToEnd();\n" +
-                "//        textBox.selectionColor = 0xFF8888FF;\n" +
-                "//        textBox.selectionUnfocusedColor = 0xFFBBBBFF;\n" +
-                "//        textBox.fontUnfocusedColor = 0xFFb59774;\n" +
-                "//\n" +
-                "//        addRenderableWidget(textBox);"));
-
-        textBlock.fontColor = 0xFFb59774;
-
-        addRenderableWidget(textBlock);
+        addRenderableWidget(textBox);
     }
 
     @Override
     public void tick() {
-//        textBox.tick();
+        textBox.tick();
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        textBox.setX(this.width / 2 - textBox.getWidth() / 2);
 
         renderBackground(guiGraphics);
         guiGraphics.fill(textBox.getX() - 4, textBox.getY() - 4,
---
+++ common/src/main/java/io/github/mortuusars/exposure/client/gui/screen/element/HorizontalAlignment.java
@@ -0,0 +1,7 @@
+package io.github.mortuusars.exposure.client.gui.screen.element;
+
+public enum HorizontalAlignment {
+    LEFT,
+    CENTER,
+    RIGHT
+}
