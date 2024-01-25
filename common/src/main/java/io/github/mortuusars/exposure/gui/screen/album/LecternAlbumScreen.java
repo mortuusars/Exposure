72a1f8a3421d9fd92ee331f8ab5e1ae27913bcd5
mortuusars <mortuusars@gmail.com>
2024-01-25 05:03:16 +0200

    Lectern is hacked together

---
+++ common/src/main/java/io/github/mortuusars/exposure/gui/screen/album/LecternAlbumScreen.java
@@ -0,0 +1,60 @@
+package io.github.mortuusars.exposure.gui.screen.album;
+
+import io.github.mortuusars.exposure.menu.AlbumMenu;
+import io.github.mortuusars.exposure.menu.LecternAlbumMenu;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.inventory.LecternScreen;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerListener;
+import net.minecraft.world.item.ItemStack;
+
+public class LecternAlbumScreen extends AlbumScreen {
+    private final LecternAlbumMenu menu;
+
+    private final ContainerListener listener = new ContainerListener() {
+        public void slotChanged(AbstractContainerMenu containerToSend, int dataSlotIndex, ItemStack stack) {
+//            LecternScreen.this.bookChanged();
+        }
+
+        public void dataChanged(AbstractContainerMenu containerMenu, int dataSlotIndex, int value) {
+            if (dataSlotIndex == 1) {
+                pager.setPage(value);
+            }
+        }
+    };
+
+    public LecternAlbumScreen(AlbumMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+        this.menu = (LecternAlbumMenu) menu;
+    }
+
+    protected boolean forcePage(int pageNum) {
+        if (pageNum != this.menu.getPage()) {
+            sendButtonClick(100 + pageNum);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        if (player.mayBuild()) {
+            addRenderableWidget(Button.builder(CommonComponents.GUI_DONE, b -> this.onClose())
+                    .bounds(this.width / 2 - 100, topPos + 196, 98, 20).build());
+            addRenderableWidget(Button.builder(Component.translatable("lectern.take_book"), b -> sendButtonClick(LecternAlbumMenu.TAKE_BOOK_BUTTON))
+                    .bounds(this.width / 2 + 2, topPos + 196, 98, 20).build());
+        }
+
+        getMenu().addSlotListener(listener);
+    }
+
+    public void removed() {
+        super.removed();
+        this.menu.removeSlotListener(this.listener);
+    }
+}
---
+++ common/src/main/java/io/github/mortuusars/exposure/mixin/BookPageCountMixin.java
@@ -0,0 +1,19 @@
+package io.github.mortuusars.exposure.mixin;
+
+import io.github.mortuusars.exposure.item.AlbumItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.WrittenBookItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WrittenBookItem.class)
+public class BookPageCountMixin {
+    @Inject(method = "getPageCount", at = @At("HEAD"), cancellable = true)
+    private static void getPageCount(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        if (stack.getItem() instanceof AlbumItem albumItem) {
+            cir.setReturnValue(albumItem.getPages(stack).size());
+        }
+    }
+}
