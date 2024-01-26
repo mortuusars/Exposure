3267d8b7cfe20a82847c0600864209f593f42bf9
mortuusars <mortuusars@gmail.com>
2024-01-26 22:20:46 +0200

    Fix creative menu items

--- forge/src/main/java/io/github/mortuusars/exposure/forge/event/CommonEvents.java
+++ forge/src/main/java/io/github/mortuusars/exposure/forge/event/CommonEvents.java
@@ -37,7 +37,6 @@ public class CommonEvents {
                 event.accept(Exposure.Items.PHOTOGRAPH.get());
                 event.accept(Exposure.Items.STACKED_PHOTOGRAPHS.get());
                 event.accept(Exposure.Items.ALBUM.get());
-                event.accept(Exposure.Items.SIGNED_ALBUM.get());
             }
 
             if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
