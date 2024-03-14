5e32970aa8c1dc5de8ed243bf07e87988fa39820
mortuusars <mortuusars@gmail.com>
2024-03-14 02:13:09 +0200

    Don't hard fail when Create version is incorrect.

---
+++ fabric/src/main/java/io/github/mortuusars/exposure/fabric/ExposureMixinPlugin.java
@@ -0,0 +1,62 @@
+package io.github.mortuusars.exposure.fabric;
+
+import com.google.common.collect.ImmutableMap;
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+
+public class ExposureMixinPlugin implements IMixinConfigPlugin {
+
+    private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
+            "io.github.mortuusars.exposure.fabric.mixin.create.CreateJEICompatMixin", ExposureMixinPlugin::isCorrectCreateVersion,
+            "io.github.mortuusars.exposure.fabric.mixin.create.RecipeTypesMixin", ExposureMixinPlugin::isCorrectCreateVersion,
+            "io.github.mortuusars.exposure.fabric.mixin.create.SpoutDevelopingMixin", ExposureMixinPlugin::isCorrectCreateVersion
+    );
+
+    private static boolean isCorrectCreateVersion() {
+        return FabricLoader.getInstance().getModContainer("create")
+                        .map(c -> c.getMetadata().getVersion().getFriendlyString().startsWith("0.5.1-f"))
+                        .orElse(false);
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return CONDITIONS.getOrDefault(mixinClassName, () -> true).get();
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
