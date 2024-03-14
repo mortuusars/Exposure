6a127e9fff21b55d448136708e38547c23c13e65
mortuusars <mortuusars@gmail.com>
2024-03-14 02:13:09 +0200

    Don't hard fail when Create version is incorrect.

--- fabric/src/main/java/io/github/mortuusars/exposure/fabric/mixin/create/RecipeTypesMixin.java
+++ fabric/src/main/java/io/github/mortuusars/exposure/fabric/mixin/create/RecipeTypesMixin.java
@@ -15,7 +15,8 @@ public class RecipeTypesMixin {
     @Inject(method = "shouldIgnoreInAutomation", at = @At("HEAD"), cancellable = true)
     private static void onShouldIgnoreInAutomation(Recipe<?> recipe, CallbackInfoReturnable<Boolean> cir) {
         if (recipe.getSerializer().equals(Exposure.RecipeSerializers.FILM_DEVELOPING.get()) ||
-                recipe.getSerializer().equals(Exposure.RecipeSerializers.PHOTOGRAPH_CLONING.get()))
+                recipe.getSerializer().equals(Exposure.RecipeSerializers.PHOTOGRAPH_CLONING.get()) ||
+                recipe.getSerializer().equals(Exposure.RecipeSerializers.PHOTOGRAPH_AGING.get()))
             cir.setReturnValue(true);
     }
 }
