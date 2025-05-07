97c99c8578e36b2b6a0b6d693d55846de7e1a490
⭐ <astraimer.ru@gmail.com>
2025-05-07 19:11:32 +0200

    Fix of my own mistake earlier.

--- fabric/src/main/java/io/github/mortuusars/exposure/fabric/mixin/AlbumItemFabricMixin.java
+++ fabric/src/main/java/io/github/mortuusars/exposure/fabric/mixin/AlbumItemFabricMixin.java
@@ -4,17 +4,16 @@ import io.github.mortuusars.exposure.world.item.AbstractAlbumItem;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = FabricItem.class, remap = false)
+@Mixin(value = Item.class, remap = false)
 public abstract class AlbumItemFabricMixin implements FabricItem {
 
-    @Inject(method = "allowComponentsUpdateAnimation", at = @At("HEAD"), cancellable = true)
-    public void allowComponentsUpdateAnimation(Player player, InteractionHand hand, ItemStack oldStack, ItemStack newStack, CallbackInfoReturnable<Boolean> cir) {
-        if (((Object) this) instanceof AbstractAlbumItem albumItemCommon) cir.setReturnValue(albumItemCommon.shouldPlayEquipAnimation(oldStack, newStack));
+    @Override
+    public boolean allowComponentsUpdateAnimation(Player player, InteractionHand hand, ItemStack oldStack, ItemStack newStack) {
+        if (((Object) this) instanceof AbstractAlbumItem albumItemCommon) return albumItemCommon.shouldPlayEquipAnimation(oldStack, newStack);
+        else return true;
     }
 }
--- neoforge/src/main/java/io/github/mortuusars/exposure/neoforge/mixin/AlbumItemNeoForgeMixin.java
+++ neoforge/src/main/java/io/github/mortuusars/exposure/neoforge/mixin/AlbumItemNeoForgeMixin.java
@@ -1,18 +1,18 @@
 package io.github.mortuusars.exposure.neoforge.mixin;
 
 import io.github.mortuusars.exposure.world.item.AbstractAlbumItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.extensions.IItemExtension;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = IItemExtension.class, remap = false)
+@Mixin(value = Item.class, remap = false)
 public abstract class AlbumItemNeoForgeMixin implements IItemExtension {
 
-    @Inject(method = "shouldCauseReequipAnimation", at = @At("HEAD"), cancellable = true)
-    public void shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged, CallbackInfoReturnable<Boolean> cir) {
-        if (((Object) this) instanceof AbstractAlbumItem albumItemCommon) cir.setReturnValue(albumItemCommon.shouldPlayEquipAnimation(oldStack, newStack));
+    @Override
+    public boolean shouldCauseReequipAnimation(@NotNull ItemStack oldStack, @NotNull ItemStack newStack, boolean slotChanged) {
+        if (((Object) this) instanceof AbstractAlbumItem albumItemCommon) return albumItemCommon.shouldPlayEquipAnimation(oldStack, newStack);
+        else return !oldStack.equals(newStack);
     }
 }
